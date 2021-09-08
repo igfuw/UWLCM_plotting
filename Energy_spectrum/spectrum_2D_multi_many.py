60e67e72c02df9fa742c8f5f3e38f2aa70cc6db3
ryba183 <pz405506@icm.edu.pl>
2021-09-08 22:47:36 +0200

    cleaning

--- Energy_spectrum/spectrum_2D_multi_many.py
+++ Energy_spectrum/spectrum_2D_multi_many.py
@@ -3,6 +3,8 @@
 import h5py
 import numpy as np
 from sys import argv
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import os
 '''
@@ -13,8 +15,8 @@ python3 spectrum_2D_multi_many.py 14400 15600 240 10 80 /home/piotr-pc/Piotr/WOR
 # velocities = ["u", "v", "w", "cloud_rw_mom3", "rv", "th", "RH", "aerosol_rw_mom3"]
 # velocities = ["u", "w", "cloud_rw_mom3", "rv", "th", "RH", "actrw_rw_mom3"]
 # velocities = ["cloud_rw_mom3", "actrw_rw_mom3"]
-velocities = ["cloud_rw_mom3"]
-# velocities = ["u"]
+#velocities = ["cloud_rw_mom3"]
+velocities = ["u"]
 
 time_start = int(argv[1])
 time_end = int(argv[2])
@@ -22,10 +24,11 @@ outfreq = int(argv[3])
 from_lvl = int(argv[4])
 to_lvl = int(argv[5])
 
-directories = argv[6:len(argv):2]
+outfile = argv[6]
+directories = argv[7:len(argv):2]
 
-labels = argv[7:len(argv):2]
-outfile = argv[8]
+labels = argv[8:len(argv):2]
+#outfile = argv[8]
 
 
 def Energy(directories, labels):
@@ -37,20 +40,23 @@ def Energy(directories, labels):
         for path in path_file:
             path_to_file = os.listdir(directory+path)
             joined = os.path.join(directory,path)
+            #print(joined.split("/", -1)[-1])
 
             Exy_avg_many = [0 for i in range(len(path_to_file))]
             Exy_avg = [0 for i in range(len(velocities))]
             # Exy_mean = [0 for i in range(len(velocities))]
 
             for file in range(len(path_to_file)):
-              w3d = h5py.File(joined+'/'+path_to_file[file] + "/timestep" + str(time_start).zfill(10) + ".h5", "r")["u"][:,:]
+              #w3d = h5py.File(joined+'/'+path_to_file[file] + "/timestep" + str(time_start).zfill(10) + ".h5", "r")["u"][:,:]
+              w3d = h5py.File(joined+'/'+joined.split("/",-1)[-1] +"_out_lgrngn"+ "/timestep" + str(time_start).zfill(10) + ".h5", "r")["u"][:,:]
               nx, nz = w3d.shape
               for vel in range(len(velocities)):
                 Exy_avg[vel] = np.zeros(int((nx+1)/2))
                 # Exy_avg_many[file][vel] =np.zeros(int((nx+1)/2))
 
               for t in range(time_start, time_end+1, outfreq):
-                filename = joined+'/'+path_to_file[file] + "/timestep" + str(t).zfill(10) + ".h5"
+                #filename = joined+'/'+path_to_file[file] + "/timestep" + str(t).zfill(10) + ".h5"
+                filename = joined+'/'+joined.split("/",-1)[-1] + "_out_lgrngn"+"/timestep" + str(t).zfill(10) + ".h5"
                 # print(filename)
 
                 for vel in range(len(velocities)):
@@ -74,15 +80,16 @@ def Energy(directories, labels):
         for path in path_file:
             Exy_mean[path] /= (time_end - time_start) / outfreq + 1
             Exy_mean[path] /= to_lvl+1 - from_lvl
-            plt.loglog(lmbd, Exy_mean[path] , linewidth=2, label=lab+"_ "+path)
+            plt.loglog(lmbd, Exy_mean[path] , linewidth=2, label=lab)
 
-    plt.loglog(lmbd, 2e-1* K**(-5./3.)*1e-8, label="-5/3" )
+    plt.loglog(lmbd, 2e-1* K**(-5./3.), label="-5/3" )
     plt.xlim(2*10**4,10**2)
     plt.xlabel("l[m]")
     plt.ylabel("PSD")
     plt.legend()
     plt.grid(True, which='both', linestyle='--')
         # plt.title("Mean PSD of w 322m<z<642m @3h")
-    plt.savefig(outfile + 'Energy_spc.png')
+    #print(outfile + 'Energy_spc.png')
+    plt.savefig(outfile + 'Energy_spc_vel_u.png')
 
 Energy(directories, labels)
